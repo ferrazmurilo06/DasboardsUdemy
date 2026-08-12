e32950b0d5b5180a6c523588459a4cf0b538780b
Murilo <muriloalbuquerqueferraz@gmail.com>
2026-08-12 09:02:40 -0300

    Exercício 3

--- Fundamentos/concatena.py
+++ Fundamentos/concatena.py
@@ -1,10 +1,13 @@
-nome = input('Qual o nome do filme: ')
-yearLaunched = int(input('Qual o ano de lançamento: '))
-noteMovie = float(input('Qual a nota do filme: '))
+# nome = input('Qual o nome do filme: ')
+# yearLaunched = int(input('Qual o ano de lançamento: '))
+# noteMovie = float(input('Qual a nota do filme: '))
 
-# Alternativa 1
-print('Dados Do Filme:')
-print('==============================')
-print("Nome do filme:",nome)
-print("Ano de lançamento:",yearLaunched)
-print("Nota do filme:",noteMovie)
+# # Alternativa 1
+# print('Dados Do Filme:')
+# print('==============================')
+# print("Nome do filme:",nome)
+# print("Ano de lançamento:",yearLaunched)
+# print("Nota do filme:",noteMovie)
+
+nome = input('Qual o seu nome: ')
+print(f'Olá, {nome}! Seja bem-vindo(a) ao nosso sistema!')
