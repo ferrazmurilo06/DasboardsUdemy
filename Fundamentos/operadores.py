69337f98a70b9184a915f4d64e3aeb625b2ed30c
Murilo <muriloalbuquerqueferraz@gmail.com>
2026-08-12 09:35:52 -0300

    novos arquivos da seção fundamentos

---
+++ Fundamentos/operadores.py
@@ -0,0 +1,17 @@
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+# Aritméticos
+sum = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+mod = num1 % num2
+expo = num1 ** num2
+
+print(f'A soma dos números é: {sum}')
+print(f'A subtração dos números é: {sub}')
+print(f'A multiplicação dos números é: {mult}')
+print(f'A divisão dos números é: {div}')
+print(f'O resto da divisão dos números é: {mod}')
+print(f'A potência dos números é: {expo}')
