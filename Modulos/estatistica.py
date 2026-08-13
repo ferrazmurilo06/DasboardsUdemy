3773440fa3ee58f44f6083250fb37d016cc1f5eb
Murilo A Ferraz <muriloalbuquerqueferraz@gmail.com>
2026-08-13 17:02:37 -0300

    adicionando novos arquivos na seção módulos

---
+++ Modulos/estatistica.py
@@ -0,0 +1,5 @@
+import statistics
+
+print("Média: ", statistics.mean([1, 2, 3, 4, 5]))
+print("Mediana: ", statistics.median([1, 2, 3, 4, 5]))
+print("Moda: ", statistics.mode([1, 2, 3, 4, 5, 5]))
---
+++ Modulos/math.py
@@ -0,0 +1,7 @@
+import math
+
+print(math.pi)  # Retorna o valor de pi
+print(f"{math.pi:.2f}")  # Retorna o valor de pi com duas casas decimais
+
+print(math.e)  # Retorna o valor de e
+print(f"{math.e:.2f}")  # Retorna o valor de e com duas casas decimais
