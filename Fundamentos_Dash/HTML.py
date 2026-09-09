f8a74cf3b55e22641e9d083d1ed6d88b0d6cc9cc
Murilo A Ferraz <muriloalbuquerqueferraz@gmail.com>
2026-09-09 15:59:36 -0300

    modificando arquivo html

--- Fundamentos_Dash/HTML.py
+++ Fundamentos_Dash/HTML.py
@@ -1,6 +1,21 @@
 from dash import Dash, dcc, html
+from datetime import datetime
 
 app = Dash(__name__)
 app.layout = html.Div([
-    html.Img(src='https://images.unsplash.com/photo-1507525428034-b723cf961d3e', style={'width': '100%'}),
+    html.Img(src='https://avatars.githubusercontent.com/u/113846713?v=4'),
+    html.Hr(),
+    html.H1('Testando o Dash'),
+    html.Span(
+        children=[
+            f'Hoje é {datetime.now().strftime("%d/%m/%Y")}',
+            html.Br(),
+            'Desenvolvido por: ', html.B('Murilo A Ferraz'),
+            html.Br(),
+            html.I('Estudante de Ciências da Computação na Universidade Estadual do Ceará (UECE)'),
+        ]
+    )
 ])
+
+if __name__ == '__main__':
+    app.run(debug=True)
